d04a8e7f86f0bb28e22afe62d24cd1580e078a6d
Junaid Malik <junmalik109@gmail.com>
2022-09-06 12:27:15 +0100

    added signalr and chat class

---
+++ Events-App-API/SignalR/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Persistence.Events;
+
+namespace Events_App_API.SignalR
+{
+    public class ChatHub : Hub
+    {
+        private readonly IMediator _mediator;
+
+        public ChatHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task SendComment(Create.Command command)
+        {
+            var comment = await _mediator.Send(command);
+
+            await Clients.Group(command.id.ToString()).SendAsync("ReceiveComment", comment);
+        }
+
+        public override async Task OnConnectionAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var activityId = httpContext.Request.Query["activityId"];
+            await Groups.AddToGroupAsync(Context.ConnectionId, activityId);
+            var result = await _mediator.Send(new List.Query { EventId = Guid.Parse(activityId) });
+            await Clients.Caller.SendAsync("LoadComments", result.Value);
+        }
+    }
+}
